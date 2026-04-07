ac0d7b9af7f8823a2c9d9145e8e687a3cfa98cb9
FARABI-XD <142730104+FARABI-XD@users.noreply.github.com>
2026-04-07 21:31:32 +0600

    Update ConsoleLog.tsx

--- ConsoleLog.tsx
+++ ConsoleLog.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { LogEntry } from '../types';
+import { LogEntry } from './types';
 import { Terminal } from 'lucide-react';
 
 interface ConsoleLogProps {
